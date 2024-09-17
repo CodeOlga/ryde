aa42e92a8df687c36cfcf75285ca527432c21fbb
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2024-09-17 10:44:26 +0300

    Map.tsx completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,3 +5,32 @@ module.exports = {
     "prettier/prettier": "error",
   },
 };
+
+// // https://docs.expo.dev/guides/using-eslint/
+// module.exports = {
+//   extends: ["expo", "prettier"],
+//   plugins: ["prettier", "import"],
+//   rules: {
+//     "prettier/prettier": "error",
+//     "import/order": [
+//       "error",
+//       {
+//         groups: [
+//           "builtin",
+//           "external",
+//           "internal",
+//           "parent",
+//           "sibling",
+//           "index",
+//           "object",
+//           "type",
+//         ],
+//         "newlines-between": "always",
+//         alphabetize: {
+//           order: "asc",
+//           caseInsensitive: true,
+//         },
+//       },
+//     ],
+//   },
+// };
